3e213837aff733394b8db6370ffdc9c0d7ae6c58
Pavel Zinin <pzinin@gmail.com>
2019-12-25 19:32:49 +0300

    init

---
+++ src/test/java/io/github/pashazz/library/LibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.pashazz.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
